25810130d7dcc8940f3ce64f7abc73dc74c76d83
Guniganti Pradeep Sai Lakshman <pradeepsailakshman.guni@gmail.com>
2025-12-11 09:23:09 +0530

    Implement DefaultValues class to show default data types
    Added a class to demonstrate default values of various data types in Java.

--- Day5/DefaultValues.java
+++ Day5/DefaultValues.java
@@ -1,2 +1,32 @@
+// package com.defaults;
 
-default values code soon...
+public class DefaultValues {
+
+    // Instance variables → JVM assigns default values
+    byte defaultByte;
+    short defaultShort;
+    int defaultInt;
+    long defaultLong;
+
+    float defaultFloat;
+    double defaultDouble;
+
+    char defaultChar;       // shows empty output because default is '\u0000'
+    boolean defaultBoolean;
+
+    public static void main(String[] args) {
+
+        DefaultValues obj = new DefaultValues();
+
+        System.out.println("Default byte value: " + obj.defaultByte);
+        System.out.println("Default short value: " + obj.defaultShort);
+        System.out.println("Default int value: " + obj.defaultInt);
+        System.out.println("Default long value: " + obj.defaultLong);
+
+        System.out.println("Default float value: " + obj.defaultFloat);
+        System.out.println("Default double value: " + obj.defaultDouble);
+
+        System.out.println("Default char value: '" + obj.defaultChar + "'");
+        System.out.println("Default boolean value: " + obj.defaultBoolean);
+    }
+}
