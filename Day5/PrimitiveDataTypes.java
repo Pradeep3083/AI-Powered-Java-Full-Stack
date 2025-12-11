02a885d6cf83b63955fefbf1014f30c7f42cc761
Guniganti Pradeep Sai Lakshman <pradeepsailakshman.guni@gmail.com>
2025-12-11 09:08:57 +0530

    Add PrimitiveDataTypes class with example variables

---
+++ Day5/PrimitiveDataTypes.java
@@ -0,0 +1,26 @@
+// package com.primitive;
+
+public class PrimitiveDataTypes {
+    public static void main(String[] args) {
+
+        byte runsPerOver = 36;
+        short oneMbInKb = 1024;
+        int youtubeSubscribers = 50000;
+        long worldPopulation = 8000000000L;
+
+        float petrolPricePerLitre = 106.75f;
+        double strikeRate = 182.345678;
+
+        char headsOrTales = 'H';
+        boolean isJavaFun = true;
+
+        System.out.println("Runs per Over: " + runsPerOver);
+        System.out.println("1 MB in KB: " + oneMbInKb);
+        System.out.println("YouTube Subscribers: " + youtubeSubscribers);
+        System.out.println("World Population: " + worldPopulation);
+        System.out.println("Petrol Price per Litre: " + petrolPricePerLitre);
+        System.out.println("Strike Rate: " + strikeRate);
+        System.out.println("Coin Toss Result (H/T): " + headsOrTales);
+        System.out.println("Is Java Fun? " + isJavaFun);
+    }
+}
