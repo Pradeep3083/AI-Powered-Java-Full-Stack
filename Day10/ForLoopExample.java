b9d5e8cebf2cf19e2ba088cacd770efe52a7adf7
Guniganti Pradeep Sai Lakshman <pradeepsailakshman.guni@gmail.com>
2025-12-18 09:36:12 +0530

    Create ForLoopExample.java

---
+++ Day10/ForLoopExample.java
@@ -0,0 +1,11 @@
+public class ForLoopExample {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Hello World " + i);
+        }
+        System.out.println("--------------");
+        for (int i = 5; i >= 0; i--) {
+            System.out.println(i + " times");
+        }
+    }
+}
