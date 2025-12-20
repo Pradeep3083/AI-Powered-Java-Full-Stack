a5006b9c9f283e0abdb593d392bc144e3d56242c
Guniganti Pradeep Sai Lakshman <pradeepsailakshman.guni@gmail.com>
2025-12-20 08:38:39 +0530

    Add CountDigitsDoWhile program to count digits

---
+++ Tasks/Loops Task/CountDigitsDoWhile.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class CountDigitsDoWhile {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int num = sc.nextInt();
+
+        int count = 0;
+
+        do {
+            num = num / 10;
+            count++;
+        } while (num > 0);
+
+        System.out.println("Digit count: " + count);
+    }
+}
