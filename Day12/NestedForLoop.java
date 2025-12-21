32c407732dad776cd02083af784053e75ff8d9f1
Guniganti Pradeep Sai Lakshman <pradeepsailakshman.guni@gmail.com>
2025-12-21 13:28:49 +0530

    Add NestedForLoop class with nested loops

---
+++ Day12/NestedForLoop.java
@@ -0,0 +1,12 @@
+package com.forloop;
+
+public class NestedForLoop {
+	public static void main(String[] args) {
+		for(int i=1;i<=4;i++) {
+			for(int j=1;j<=4;j++) {
+				System.out.println("i: "+i);
+				System.out.println("j: "+j);
+			}
+		}
+	}
+}
