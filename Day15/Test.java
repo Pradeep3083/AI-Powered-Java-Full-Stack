d5835199ccfe6a14bf9ca52c1c38cadf89311c5a
Guniganti Pradeep Sai Lakshman <pradeepsailakshman.guni@gmail.com>
2025-12-25 20:22:41 +0530

    Enhance string comparison examples in Test.java
    Added string comparison examples and explanations.

--- Day15/Test.java
+++ Day15/Test.java
@@ -11,7 +11,26 @@ public class Test {
 		System.out.println(s1); // Here it will create a new one and where "Pradeep" will be in the background only
 		// After the GC will collect it.
 		
-		// code soon
+		String s3 = "PradeepSai";
+		String s4 = "pradeepsai";
+		// here s3 & s4 both are not same
+		
+		String s5 = new String("Psl");
+		String s6 = new String("Psl");
+		// when we create an object it will take a new memory in the heap area
+		// It does not care about the value it will store in the new memory only
+		
+		
+		System.out.println(s1==s2);
+		System.out.println(s3==s4);
+		System.out.println(s5==s6); // false
+		
+		System.out.println(s5.equals(s6)); // true
+		// for comparing string we have to use .equals() method
+		
+		
+		
+		
 		
 		
 	}
