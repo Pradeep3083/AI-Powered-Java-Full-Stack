a7c24187512f42f67249a83da23e5211a4a3e1ff
Guniganti Pradeep Sai Lakshman <pradeepsailakshman.guni@gmail.com>
2025-12-31 11:20:45 +0530

    Update ReverseArray.java

--- Day18/ReverseArray.java
+++ Day18/ReverseArray.java
@@ -3,10 +3,17 @@ package com.programs;
 public class ReverseArray {
 	public static void main(String[] args) {
 		int[] arr = {10,20,15,28,34};
-		int newArr[] = reverseArray(arr);
-		for(int i=0;i<newArr.length;i++) {
-			System.out.print(newArr[i]+" ");
-		}
+		//int[] revArray = reverseArray(arr);
+		int[] newArr2 = reverseArray2(arr);
+		
+//		for(int i=0;i<revArray.length;i++) {
+//			System.out.print(revArray[i]+" ");
+//		}
+		
+		// Enhanced For Loop / for each
+		for(int num : newArr2) {
+			System.out.print(num+" ");
+		}		
 	}
 	
 	static int[] reverseArray(int[] arr){
@@ -19,4 +26,18 @@ public class ReverseArray {
 		}
 		return newArr;
 	}
+	
+	static int[] reverseArray2(int[] arr) {
+		int start = 0;
+		int end = arr.length-1;
+		while(start <= arr.length/2) {	
+			int temp = arr[start];
+			arr[start] = arr[end];
+			arr[end] = temp;
+			start++;
+			end--;
+		}
+		return arr;
+	}
+	
 }
