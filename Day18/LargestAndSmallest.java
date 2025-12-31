ad9b101c68df93993a957b32d2ef9a43bd2aba0d
Guniganti Pradeep Sai Lakshman <pradeepsailakshman.guni@gmail.com>
2025-12-31 10:43:37 +0530

    Update LargestAndSmallest.java

--- Day18/LargestAndSmallest.java
+++ Day18/LargestAndSmallest.java
@@ -4,11 +4,16 @@ public class LargestAndSmallest {
 	public static void main(String[] args) {
 		int[] arr = {13,5,17,8,23,15};
 		int max = arr[0];
+		int min = arr[0];
 		for(int i=0;i<arr.length;i++) {
 			if(arr[i]>max) {
 				max = arr[i];
 			}
+			if(arr[i]<max) {
+				min = arr[i];
+			}
 		}
-		System.out.println(max);
+		System.out.println("Largest Number: "+max);
+		System.out.println("Smalles Number: "+min);
 	}
 }
