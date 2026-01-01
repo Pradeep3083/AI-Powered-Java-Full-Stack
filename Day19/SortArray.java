fc396256bea14dc84b20247bc24b245a41aed07a
Guniganti Pradeep Sai Lakshman <pradeepsailakshman.guni@gmail.com>
2026-01-01 11:09:20 +0530

    Update SortArray.java

--- Day19/SortArray.java
+++ Day19/SortArray.java
@@ -4,24 +4,27 @@ public class SortArray {
 	public static void main(String[] args) {
 		int[] arr = {10,2,12,11,8};
 		
+		System.out.println("Before Sorting: ");
 		for(int num : arr) {
 			System.out.print(num+" ");
 		}
 		
 		for(int i=0;i<arr.length-1;i++) {
 			for(int j=0;j<arr.length-1;j++) {
-				if(arr[i]<arr[j]) {
-					int temp = arr[i];
-					arr[i] = arr[j];
-					arr[j] = temp;
+				if(arr[j]>arr[i]) {
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
 				}
 			}
 		}
+		
 		System.out.println();
+		
+		System.out.println("After Sorting: ");
 		for(int num : arr) {
 			System.out.print(num+" ");
-		}
-		
+		}		
 		
 	}
 }
