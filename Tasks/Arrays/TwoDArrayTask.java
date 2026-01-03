f25cc6d051fa51685e1669dc579a327c8e12f2f3
Guniganti Pradeep Sai Lakshman <pradeepsailakshman.guni@gmail.com>
2026-01-03 08:39:07 +0530

    Update TwoDArrayTask.java

--- Tasks/Arrays/TwoDArrayTask.java
+++ Tasks/Arrays/TwoDArrayTask.java
@@ -4,12 +4,10 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class TwoDArrayTask {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {		
 		Scanner sc = new Scanner(System.in);
 		
-		// Array with 3 X 5
-		int[][] arr = new int[3][5];
+		int[][] arr = new int[3][5]; // Array with 3 X 5
 		
 		System.out.println("Enter Elements: ");
 		
@@ -22,14 +20,17 @@ public class TwoDArrayTask {
 		
 		for(int[] num : arr) {
 			System.out.println(Arrays.toString(num));
-		}
-		
-//		for(int i=0;i<arr.length;i++) {
-//			for(int j=0;j<arr[0].length;j++) {
-//				System.out.print(arr[i][j]+" ");
-//			}
-//			System.out.println();
-//		}
+		}		
+
+		//for(int i=0;i<arr.length;i++) {
+		//for(int j=0;j<arr[0].length;j++) {
+		//	System.out.print(arr[i][j]+" ");
+		//}
+		//System.out.println();
+		//}
 		
 	}
 }
+
+
+
