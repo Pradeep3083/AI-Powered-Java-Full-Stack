162a7b0a10591a55b5827c377ed3d897003daeb0
Guniganti Pradeep Sai Lakshman <pradeepsailakshman.guni@gmail.com>
2026-01-04 09:19:10 +0530

    Create SecondLargest.java

---
+++ Tasks/Arrays/SecondLargest.java
@@ -0,0 +1,28 @@
+package com.programs;
+
+public class SecondLargest {
+	public static void main(String[] args) {
+		
+		int[] arr = {10,25,16,17,20};
+		
+		int largest = arr[0];
+		int secondLargest = arr[0];
+		
+		for(int i=1;i<arr.length;i++) {
+			if(arr[i] > largest) {
+				largest = arr[i];
+			}
+		}
+		
+		for(int i=0;i<arr.length;i++) {
+			if(arr[i]!=largest) {
+				if(secondLargest == largest || arr[i] > secondLargest) {
+					secondLargest = arr[i];
+				}
+			}
+		}
+		
+		System.out.println("Second Largest Number: "+secondLargest);
+		
+	}
+}
