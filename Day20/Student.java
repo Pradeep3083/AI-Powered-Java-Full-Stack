aea74216a47f6f5e5c4f965cb404468a66611ae3
Guniganti Pradeep Sai Lakshman <pradeepsailakshman.guni@gmail.com>
2026-01-08 08:38:08 +0530

    Create Student.java

---
+++ Day20/Student.java
@@ -0,0 +1,13 @@
+package com;
+
+public class Student {
+	public static void main(String[] args) {
+		String studentName = "Pradeep";
+		
+		int studentId = 1;
+		
+		String studentAge = "Male";
+		
+		int studentGender = 23;
+	}
+}
