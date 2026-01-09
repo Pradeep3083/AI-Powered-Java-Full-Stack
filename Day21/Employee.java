f982096a7dcfc23afac0c6ce4c9c12a313ffa619
Guniganti Pradeep Sai Lakshman <pradeepsailakshman.guni@gmail.com>
2026-01-09 19:05:09 +0530

    Create Employee.java

---
+++ Day21/Employee.java
@@ -0,0 +1,26 @@
+package com;
+
+public class Employee {
+
+    // Instance variables (Stored in Heap – per object)
+    int id;
+    String name;
+
+    // Static variable (Stored in Method Area – shared)
+    static String companyName = "FLM";
+
+}
+
+
+
+/*
+package com;
+
+public class Employee {
+	String name;
+	int id;
+	static String companyName;
+}
+*/
+
+
