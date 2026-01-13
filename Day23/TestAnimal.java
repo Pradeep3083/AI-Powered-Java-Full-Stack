6c72f610f86084aa66de0813ec44b801e8dd8c9c
Guniganti Pradeep Sai Lakshman <pradeepsailakshman.guni@gmail.com>
2026-01-13 08:16:53 +0530

    Create TestAnimal.java

---
+++ Day23/TestAnimal.java
@@ -0,0 +1,21 @@
+package com;
+
+public class TestAnimal {
+	public static void main(String[] args) {
+//		Dog dog = new Dog();
+//		dog.sound();
+//		dog.print();
+		
+		Calculator calc = new Calculator();
+		
+		calc.sum(1, 2);
+		calc.sum(1, 2, 3);
+		main(10);
+		
+	}
+	
+	public static void main(int a) {
+		
+	}
+	
+}
