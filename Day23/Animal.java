1d958a49eef562b08ed424f6468890fd91cb70e2
Guniganti Pradeep Sai Lakshman <pradeepsailakshman.guni@gmail.com>
2026-01-13 08:16:31 +0530

    Create Animal.java

---
+++ Day23/Animal.java
@@ -0,0 +1,17 @@
+package com;
+
+public class Animal {
+	
+	int a = 10;
+	
+	int numOfLegs;
+	
+	public Animal() {
+		System.out.println("Animal Object created...");
+	}
+	
+	void walk() {
+		System.out.println("Animal is walking...");
+	}
+	
+}
