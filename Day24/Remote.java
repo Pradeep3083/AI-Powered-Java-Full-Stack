f85dc3ec1c78936c6e402a2612179ed83ca43b9b
Guniganti Pradeep Sai Lakshman <pradeepsailakshman.guni@gmail.com>
2026-01-15 10:26:04 +0530

    Update Remote.java

--- Day24/Remote.java
+++ Day24/Remote.java
@@ -1,7 +1,16 @@
 package overloading;
 
+// Parent Class
 public class Remote {
+	
+	// Instance Method (can be overriden)
 	void turnOn() {
 		System.out.println("Turning on...");
 	}
+	
+	// Static method (cannot be overriden)
+	static void turnOff() {
+		System.out.println("Turning off...");
+	}
+	
 }
