a5a7d5d1a0ec679e96d47b5666471ec903a8c1fe
Guniganti Pradeep Sai Lakshman <pradeepsailakshman.guni@gmail.com>
2026-01-16 08:38:46 +0530

    Update Calculator.java

--- Day23/Calculator.java
+++ Day23/Calculator.java
@@ -1,11 +1,14 @@
 package com;
 
+// Method Overloading Example
 public class Calculator {
 	
+	// Method with two parameters
 	void sum(int a, int b) {
 		System.out.println(a+b);
 	}
 	
+	// Same method name, different parameters
 	void sum(int a, int b, int c) {
 		System.out.println(a+b+c);
 	}
