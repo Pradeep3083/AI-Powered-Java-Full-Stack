bde65c827946163fc66917c4b811a284c310a031
Guniganti Pradeep Sai Lakshman <pradeepsailakshman.guni@gmail.com>
2026-01-16 08:39:04 +0530

    Update Animal.java

--- Day23/Animal.java
+++ Day23/Animal.java
@@ -1,15 +1,18 @@
 package com;
 
+// Parent Class
 public class Animal {
 	
 	int a = 10;
 	
 	int numOfLegs;
 	
-	public Animal() {
+	// Constructor
+	Animal() {
 		System.out.println("Animal Object created...");
 	}
 	
+	// Instance method
 	void walk() {
 		System.out.println("Animal is walking...");
 	}
