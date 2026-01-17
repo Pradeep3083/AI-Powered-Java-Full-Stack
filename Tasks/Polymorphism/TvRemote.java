48082c5863d299f354c001698f47da2f19fc4787
Guniganti Pradeep Sai Lakshman <pradeepsailakshman.guni@gmail.com>
2026-01-17 07:23:17 +0530

    Create TvRemote.java

---
+++ Tasks/Polymorphism/TvRemote.java
@@ -0,0 +1,37 @@
+package overloading;
+
+// Child class
+public class TvRemote extends Remote{
+	
+	// Instance variable
+	int volume = 0;
+	
+	// Method overriding
+	@Override
+	void turnOn() {
+		System.out.println("Samsung tv turning on...");
+	}
+	
+	// Method overloading
+	void turnOn(int a) {
+		System.out.println(a);
+	}
+	
+	// static method hiding (not overriding)
+	static void turnOff() {
+		System.out.println("Samsung tv turned off...");
+	}
+	
+	// Increase volume by 1
+	void increaseVolume() {
+		this.volume = this.volume+1;
+		System.out.println("Volume : "+volume);
+	}
+	
+	// Overload method
+	void increaseVolume(int volume) {
+		this.volume += volume;
+		System.out.println("Volume : "+this.volume);
+	}
+}
+
