b62810962cb5307da7cbc9cbefc6d1bfd9b2f630
Guniganti Pradeep Sai Lakshman <pradeepsailakshman.guni@gmail.com>
2026-01-18 08:14:13 +0530

    Create Animal.java

---
+++ Tasks/Polymorphism/Day23 Codes/Animal.java
@@ -0,0 +1,20 @@
+package com;
+
+// Parent Class
+public class Animal {
+	
+	int a = 10;
+	
+	int numOfLegs;
+	
+	// Constructor
+	Animal() {
+		System.out.println("Animal Object created...");
+	}
+	
+	// Instance method
+	void walk() {
+		System.out.println("Animal is walking...");
+	}
+	
+}
