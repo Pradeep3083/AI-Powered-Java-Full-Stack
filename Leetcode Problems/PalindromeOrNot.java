feed830b8f32fa04c16dadcf6c34fb646686ab80
Guniganti Pradeep Sai Lakshman <pradeepsailakshman.guni@gmail.com>
2026-01-19 09:48:16 +0530

    Add PalindromeOrNot class with palindrome check method

---
+++ Leetcode Problems/PalindromeOrNot.java
@@ -0,0 +1,24 @@
+class PalindromeOrNot {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        int left = 0, right = s.length() - 1;
+
+        while (left < right) {
+            char l = s.charAt(left);
+            char r = s.charAt(right);
+
+            if (!Character.isLetterOrDigit(l)) {
+                left++;
+            } else if (!Character.isLetterOrDigit(r)) {
+                right--;
+            } else {
+                if (l != r) {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+        }
+        return true;
+    }
+}
