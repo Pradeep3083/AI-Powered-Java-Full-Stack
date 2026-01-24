be5ffcfa7c6160bd3c90b4d106ef7d1f6f00beee
Guniganti Pradeep Sai Lakshman <pradeepsailakshman.guni@gmail.com>
2026-01-24 10:56:27 +0530

    Add Car class implementing Vehicle interface

---
+++ Day26/Car.java
@@ -0,0 +1,13 @@
+package com;
+
+public class Car implements Vehicle{
+	@Override
+	public void start() {
+		System.out.println("Car started");
+	}
+	
+	@Override
+	public void stop() {
+		System.out.println("Car stopped");
+	}
+}
