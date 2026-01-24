6f9c090fffff4b937569129abaf33e89a33ec62b
Guniganti Pradeep Sai Lakshman <pradeepsailakshman.guni@gmail.com>
2026-01-24 10:54:08 +0530

    Create Test.java

---
+++ Day26/Test.java
@@ -0,0 +1,13 @@
+package com;
+
+public class Test {
+	public static void main(String[] args) {
+		Dog dog = new Dog();
+		dog.sound();
+		dog.sleep();
+		
+		Car car = new Car();
+		car.start();
+		car.stop();
+	}
+}
