2d4511640afc1fbd5dd831bd4d221f6f144716ab
Guniganti Pradeep Sai Lakshman <pradeepsailakshman.guni@gmail.com>
2026-01-28 22:09:54 +0530

    Refactor WrapperDemo with package and examples
    Reorganized package declaration and added auto-boxing/unboxing examples.

--- Day29/WrapperDemo.java
+++ Day29/WrapperDemo.java
@@ -1,10 +1,12 @@
+package com;
+
 public class WrapperDemo {
-
-    static Integer num; // Wrapper default value = null
-
-    public static void main(String[] args) {
-
-        int a = 10;              // primitive
+	
+	static Integer num;
+	
+	public static void main(String[] args) {		
+		
+		int a = 10;              // primitive
         Integer b = a;           // auto-boxing
         int c = b;               // auto-unboxing
 
@@ -18,6 +20,55 @@ public class WrapperDemo {
         System.out.println(Integer.min(5, 3));
 
         System.out.println(Character.isDigit('1'));
-        System.out.println(Character.isUpperCase('A'));
-    }
+        System.out.println(Character.isUpperCase('A'));				
+		
+	}
 }
+
+
+/*
+
+int a = 10;
+
+Integer b = new Integer(20);
+Integer c = Integer.valueOf(20);
+
+// Java 5
+Integer d = Integer.valueOf(a); // Primitive to Wrapper
+int e = c.intValue(); // Wrapper to Primitive
+// intValue() is instance method
+
+// After Java 5
+Integer f = e; // Auto Boxing
+int g = f; // Auto Unboxing
+
+System.out.println(num);	
+
+Integer num = 10;
+String num2 = "20";
+System.out.println(num2 + 2);
+System.out.println(num + 2);
+
+int num3 = Integer.parseInt(num2);
+System.out.println(num3 + 2);
+
+System.out.println(Integer.max(10, 12));
+System.out.println(Integer.min(10,5));
+
+String num5 = String.valueOf(10);
+System.out.println(num5+2);
+
+Character char1 = Character.valueOf('A');
+System.out.println(Character.isUpperCase('A'));
+System.out.println(Character.isUpperCase('a'));
+System.out.println(Character.isDigit('1'));
+System.out.println(Character.isLetter('A'));
+System.out.println(Character.toUpperCase('a'));
+System.out.println(Character.isWhitespace(' '));
+
+Integer num6 = 10;
+System.out.println(num6.hashCode());
+num6 = 12;
+System.out.println(num6.hashCode());
+
+*/
